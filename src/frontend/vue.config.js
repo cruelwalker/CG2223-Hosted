80ed919c9946f8195f0fe5d311e62924e48a6ecf
Cruelwalker <rasheedhamdani93@gmail.com>
2023-06-10 23:03:07 +0200

    port change

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     port: 8081,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:56192',
         ws: true,
         changeOrigin: true
       }
