07a276a117e6997f0556cd0fe9e10fef5a282519
Cruelwalker <rasheedhamdani93@gmail.com>
2023-06-10 22:50:43 +0200

    changed port

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   // https://cli.vuejs.org/config/#devserver-proxy
   devServer: {
-    port: 3000,
+    port: 8081,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
